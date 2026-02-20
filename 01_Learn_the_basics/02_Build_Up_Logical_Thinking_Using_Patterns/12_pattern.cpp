b3e20f16a2daa5e24cab44b6097a6519bae8949d
Anurag Shukla <151445414+AnuragShukla19@users.noreply.github.com>
2026-02-20 10:20:22 +0530

    Create 12_pattern.cpp

---
+++ 01_Learn_the_basics/02_Build_Up_Logical_Thinking_Using_Patterns/12_pattern.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern12(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<j+1<<" ";
+        }
+        for(int j=0;j<2*n-2*(i+1);j++){
+            cout<<"  ";
+        }
+        for(int j=i;j>=0;j--){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern12(n);
+    return 0;
+}
