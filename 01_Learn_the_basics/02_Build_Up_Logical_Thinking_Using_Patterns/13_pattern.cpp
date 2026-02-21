3a6e82296834eed8e85218674df03883ed078395
Anurag Shukla <151445414+AnuragShukla19@users.noreply.github.com>
2026-02-21 07:36:51 +0530

    Create 13_pattern.cpp

---
+++ 01_Learn_the_basics/02_Build_Up_Logical_Thinking_Using_Patterns/13_pattern.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern13(int n){
+    int start=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<start<<" ";
+            start++;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern13(n);
+    return 0;
+}
